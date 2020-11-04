6081224891d825dbacb2f08d4e4c1ef3287f6320
Caroline Bastos <caroline.bastos@celfocus.com>
2020-11-04 13:15:01 +0000

    desafio fundamentos nodejs

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface createTransationDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(function (acumulador, valorAtual) {
+      if (valorAtual.type === "income"){
+         acumulador = acumulador + valorAtual.value
+      }
+      return acumulador;
+    }, 0);
+    const outcome =  this.transactions.reduce(function (acumulador, valorAtual) {
+      if (valorAtual.type === "outcome"){
+        acumulador = acumulador + valorAtual.value
+      }
+      return acumulador;
+    }, 0);
+
+    const balance: Balance = {
+      income: income,
+      outcome: outcome,
+      total: income -outcome
+    }
+
+    return balance;
+    
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: createTransationDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
